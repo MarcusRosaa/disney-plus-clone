85fe6ac350e20c7aadb8e2495ae91e17441af091
MarcusRosaa <marcusvrosa1@gmail.com>
2021-10-13 19:17:52 -0300

    add nav menus and hover effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,28 @@ const Header = (props) => {
       <NavMenu>
         <a href="/home">
           <img src="/images/home-icon.svg" alt="HOME" />
+          <span>HOME</span>
         </a>
-        <span>HOME</span>
+        <a href="/search">
+          <img src="/images/search-icon.svg" alt="HOME" />
+          <span>SEARCH</span>
+        </a>
+        <a href="/watchlist">
+          <img src="/images/watchlist-icon.svg" alt="HOME" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="/originals">
+          <img src="/images/original-icon.svg" alt="HOME" />
+          <span>ORIGINALS</span>
+        </a>
+        <a href="/movies">
+          <img src="/images/movie-icon.svg" alt="HOME" />
+          <span>MOVIES</span>
+        </a>
+        <a href="/series">
+          <img src="/images/series-icon.svg" alt="HOME" />
+          <span>SERIES</span>
+        </a>
       </NavMenu>
     </Nav>
   );
@@ -57,9 +77,59 @@ const NavMenu = styled.div`
   margin-right: auto;
   margin-left: 25px;
 
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0;
+      white-space: nowrap;
+      position: relative;
+
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0 0 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0;
+        opacity: 0;
+        position: absolute;
+        right: 0;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+
+  }
+  
+
   @media (max-width:768px) {
     display: none;
   }
-`
+  `;
 
 export default Header;
